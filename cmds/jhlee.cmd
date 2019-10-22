bf8565252044f7c9887b77bfac185b7a8b6164e3
Jeong Han Lee <jeonghan.lee@gmail.com>
2019-10-22 15:22:21 +0200

    add the changes of startup script for ADSpinnakar

--- cmds/jhlee.cmd
+++ cmds/jhlee.cmd
@@ -11,9 +11,9 @@ epicsEnvSet("CAMERA_ID", "19299225")
 epicsEnvSet("AREA", "labs-utg-test")
 epicsEnvSet("DEVICE", "cam1")
 
-epicsEnvSet("PREFIX", "$(AREA):$(DEVICE):")
-epicsEnvSet("IOCNAME", "$(AREA)-$(DEVICE)")
-epicsEnvSet("IMAGE", "Image1")
+epicsEnvSet("PREFIX",	"$(AREA):$(DEVICE):")
+epicsEnvSet("IOCNAME",	"$(AREA)-$(DEVICE)")
+epicsEnvSet("IMAGE",	"image1:")
 
 epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES","64000000")
 
@@ -40,24 +40,41 @@ set_requestfile_path("$(calc_DB)", "")
 set_requestfile_path("$(busy_DB)", "")
 set_requestfile_path("$(TOP)", "cmds")
 
-#-
+#######################################################################
+### Load ADSpinnaker                                                ###
+#######################################################################
 ADSpinnakerConfig("$(PORT)", "$(CAMERA_ID)", 0x1, 0)
 dbLoadRecords("spinnaker.db", "P=$(PREFIX),R=,PORT=$(PORT)")
 dbLoadRecords("PGR_BlackflyS_50S5C.db", "P=$(PREFIX),R=,PORT=$(PORT)")
 
-# Create a standard arrays plugin
-NDStdArraysConfigure("$(IMAGE)", 5, 0, "$(PORT)", 0, 0)
-dbLoadRecords("NDStdArrays.template", "P=$(PREFIX),R=,PORT=$(IMAGE),ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int16,FTVL=SHORT,NELEMENTS=$(NELEMENTS)")
-# Load the commPlugins.iocsh for ADCore
-iocshLoad("$(ADCore_DIR)/commPlugins.iocsh", "P=$(PREFIX),UNIT=1,PORT=$(IMAGE),QSIZE=$(QSIZE),XSIZE=$(XSIZE),YSIZE=$(YSIZE),NCHANS=$(NCHANS),CBUFFS=$(CBUFFS)")
+#######################################################################
+### Create a standard arrays plugin                                 ###
+#######################################################################
+###NDStdArraysConfigure("$(IMAGE):", 5, 0, "$(PORT)", 0, 0)
+###dbLoadRecords("NDStdArrays.template", "P=$(PREFIX),R=,PORT=$(IMAGE),ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int16,FTVL=SHORT,NELEMENTS=$(NELEMENTS)")
+NDStdArraysConfigure("Image", 5, 0, "$(PORT)", 0, 0)
+dbLoadRecords("NDStdArrays.template", "P=$(PREFIX),R=$(IMAGE),PORT=Image,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int16,FTVL=SHORT,NELEMENTS=$(NELEMENTS)")
 
-# Load autosave iocsh
+#######################################################################
+### Load the commPlugins.iocsh for ADCore                           ###
+#######################################################################
+###iocshLoad("$(ADCore_DIR)/commPlugins.iocsh", "P=$(PREFIX),UNIT=1,PORT=$(IMAGE),QSIZE=$(QSIZE),XSIZE=$(XSIZE),YSIZE=$(YSIZE),NCHANS=$(NCHANS),CBUFFS=$(CBUFFS)")
+iocshLoad("$(ADCore_DIR)/commPlugins.iocsh", "P=$(PREFIX),UNIT=1,PORT=Image,QSIZE=$(QSIZE),XSIZE=$(XSIZE),YSIZE=$(YSIZE),NCHANS=$(NCHANS),CBUFFS=$(CBUFFS)")
+
+#######################################################################
+### Load autosave iocsh                                             ###
+#######################################################################
 iocshLoad("$(autosave_DIR)/autosave.iocsh", "AS_TOP=$(TOP),IOCNAME=$(IOCNAME)")
 
-#
+#######################################################################
+### iocInit()                                                       ###
+#######################################################################
 iocInit()
 
-create_monitor_set("auto_settings.req", 5, "P=$(PREFIX),R=,IMAGE=$(IMAGE):")
+#######################################################################
+### Create monitor set                                              ###
+#######################################################################
+create_monitor_set("auto_settings.req", 5, "P=$(PREFIX),R=,IMAGE=$(IMAGE)")
 #create_monitor_set("$(TOP)/cmds/auto_settings.req", 5, "P=$(PREFIX),R=,IMAGE=$(IMAGE):")
 # Wait for enum callbacks to complete
 epicsThreadSleep(2.0)
